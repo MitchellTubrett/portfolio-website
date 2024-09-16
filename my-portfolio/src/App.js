ad60e84bf613246f2243c7b2c0a527db82f73619
Mitch Tubrett <mitchelltubrett@gmail.com>
2024-09-16 16:13:28 -0300

    Added social icons, linked and name. Fixed top bar position to remain

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
+  const name = "Mitchell Tubrett";
+  const linkedIn_link = "https://www.linkedin.com/in/mitchell-tubrett/";
+  const github_link = "https://github.com/MitchellTubrett";
   return (
     <div className="App">
-        <p>
-          Mitchell Tubrett
-        </p>
+      <div className='portfolio-bar'>
+      <p>
+          {name} 
+      </p>
+      <div className="social-icons">
+        <a href={linkedIn_link} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href={github_link} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
+      </div>
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="logo" />
