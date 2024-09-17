b951bcac92517a9c1dbeeaa3d98bca19ed8c2691
Mitch Tubrett <mitchelltubrett@gmail.com>
2024-09-17 14:21:28 -0300

    fixed background image to static, added place for content below

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -7,22 +7,31 @@ function App() {
   const name = "Mitchell Tubrett";
   const linkedIn_link = "https://www.linkedin.com/in/mitchell-tubrett/";
   const github_link = "https://github.com/MitchellTubrett";
+
   return (
-    <div className="App">
-      <div className='portfolio-bar'>
-      <p>
-          {name} 
-      </p>
-        <div className="social-icons">
-          <a href={linkedIn_link} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a href={github_link} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
+    <div className="App">   
+      <div className="background-content">
+        <div className='portfolio-bar'>
+        <p>
+            {name} 
+        </p> 
+          <div className="social-icons">
+            <a href={linkedIn_link} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href={github_link} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>          
+          </div>
+                      
         </div>
-      </div>
-    </div>
+      </div>  
+      <div className="page-content">
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is where new content can be added below the background.</p>
+            <p>Additional sections can go here.</p>
+        </div>     
+    </div>      
   );
 }
 
